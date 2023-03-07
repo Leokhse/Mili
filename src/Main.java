4c20a6f081560f1fe7332e3ea79378b98fed9f30
Leo <levan2312@yandex.ru>
2023-03-08 00:46:51 +0400

    gitignore added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 13676.545;
+        int miles = (int) (ticketPrice / 20);
+        System.out.println("Количество начисленных миль: " + miles);
+
+    }
+}
